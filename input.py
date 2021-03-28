013f37d1e2240d32e6a701d9ee7ccb6a1add2d9c
Michele Pipicelli <michelepipicellii@gmail.com>
2021-03-28 20:27:07 +1030

    Add files
    Add new files
    strings.py broken

--- input.py
+++
@@ -1,9 +0,0 @@
-name = input('What is your name? ') # Receives input from the user. Unless method is used
-                                    # Before it, it is a string
-color = input('What is your favorite color? ')
-
-print(name, 'likes', color)
-
-birth_year = input('Birth Year: ')
-age = 2021 - int(birth_year)
-print(age)
