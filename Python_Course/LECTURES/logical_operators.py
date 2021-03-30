e3c8327e4985781eb258bb98d5109ba00a1b6e89
Michele Pipicelli <michelepipicellii@gmail.com>
2021-03-30 16:44:41 +1030

    modified:   if_statements.py
    	new file:   logical_operators.py
    	modified:   math_functions.py

---
+++ Python_Course/LECTURES/logical_operators.py
@@ -0,0 +1,9 @@
+# LOGICAL OPERATORS
+
+has_high_income = True
+has_good_credit = False
+
+if has_high_income or has_good_credit:
+    print('Eligible for loan')
+else:
+    print('Ineligible for loan')
--- Python_Course/LECTURES/math_functions.py
+++ Python_Course/LECTURES/math_functions.py
@@ -1,3 +1,7 @@
+import math # Imports the math library
+
 x = 2.9
 print(round(x)) # Returns the variable rounded to the nearest integer
 print(abs(-2.9)) # Returns the absolute value of the variable, whether it is positive or negative.
+print(math.ceil(2.9)) # Returns the ceil value of the variable
+print(math.floor(2.9)) # Returns the floor value of the variable
