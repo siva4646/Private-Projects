c93a82a6e926e481be5b94cc20a69a7f3b693099
Michele Pipicelli <michelepipicellii@gmail.com>
2021-03-30 17:17:54 +1030

    modified:   LECTURES/comparison_operators.py 	new file:   LECTURES/while_loops.py 	new file:   PROJECTS/.DS_Store 	new file:   PROJECTS/Weight Converter/weight_converter_program.py

---
+++ Python_Course/PROJECTS/Weight Converter/weight_converter_program.py
@@ -0,0 +1,10 @@
+from math import ceil
+
+weight = float(input("Weight: "))
+system = str(input("(L)bs or (K)g: "))
+system = system.lower()
+
+if system == 'l': 
+    print(f'You are {weight * 0.45} kilograms')
+elif system == 'k':
+    print(f'You are {weight / 0.45} pounds')
