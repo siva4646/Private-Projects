f7ece5e7bc1f27cb44681769a0cb22584c83bc18
Michele Pipicelli <PIPICM25@smc.sa.edu.au>
2021-04-01 14:29:58 +1030

    Create converters.py

---
+++ Python_Course/PROJECTS/Private Projects/Computer/converters.py
@@ -0,0 +1,11 @@
+def mhz_to_ghz(frequency):
+  return frequency / 1000
+
+def ghz_to_mhz(frequency):
+  return frequency * 1000
+
+def kg_to_lbs(weight):
+  return weight * 0.45
+
+def lbs_to_kg(weight):
+  return weight / 0.45
