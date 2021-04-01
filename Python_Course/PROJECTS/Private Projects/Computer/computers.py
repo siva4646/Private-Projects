05cccdeb05dadc185b09babfa755eccfc00418ff
Michele Pipicelli <michelepipicellii@gmail.com>
2021-04-01 16:26:35 +1030

    Major overhaul of system. Add weight + frequency conversions. TODO: Add try/except blocks.

--- Python_Course/PROJECTS/Private Projects/Computer/computers.py
+++ Python_Course/PROJECTS/Private Projects/Computer/computers.py
@@ -1,23 +1,52 @@
+import converters
+
 graphics = {
     "geforce": "GeForce",
     "nvidia": "Nvidia"
 }
 
-gpu = str(input())
-weight = float(input())
-model = input()
-cpufreq = int(input())
+gpu = str(input("GPU Type: "))
+weight = float(input("Weight: "))
+metric = input("(L)bs or (K)g: ")
 
+if metric.lower() == 'l':
+    metric = 'lbs'
+    choice = input("Would you like to convert to kg? (Y or N): ")
+    if choice.lower() == 'y':
+        weight = converters.lbs_to_kg(weight)
+        metric = 'kg'
+elif metric.lower() == 'k':
+    metric = 'kg'
+    choice = input("Would you like to convert to lbs? (Y or N): ")
+    if choice.lower() == 'y':
+        weight = converters.kg_to_lbs(weight)
+        metric = 'lbs'
+
+model = input("Model: ")
+cpufreq = int(input("CPU Frequency: "))
+cpu_metric = input("(G)hz or (M)hz: ")
+
+if cpu_metric.lower() == 'g':
+    cpu_metric = "GHz"
+    if input("Would you like to convert to MHz? (Y or N): ").lower() == 'y':
+        cpufreq = converters.ghz_to_mhz(cpufreq)
+        cpu_metric = "MHz"
+elif cpu_metric.lower() == 'm':
+    cpu_metric = 'MHz'
+    if input("Would you like to convert to GHz? (Y or N): ").lower() == 'y':
+        cpufreq = converters.mhz_to_ghz(cpufreq)
+        cpu_metric = "GHz"
+    
 
 gpu = gpu.lower()
 
 
 if gpu in graphics: # fixed
     gpu = graphics.get(gpu)
-    output = f'{cpufreq}MHz {gpu} {weight}kg {model}'
-    cpu = f'CPU Speed: {str(cpufreq)}MHz'
+    output = f'{cpufreq}{cpu_metric} {gpu} {weight}{metric} {model}'
+    cpu = f'CPU Speed: {str(cpufreq)}{cpu_metric}'
     model = f'Model: {model}'
-    weight = f'Weight: {weight}kg'
+    weight = f'Weight: {weight}{metric}'
     gpu = f'GPU Type: {gpu}'
     
     print(f'''
--- Python_Course/PROJECTS/Private Projects/Computer/converters.py
+++ Python_Course/PROJECTS/Private Projects/Computer/converters.py
@@ -5,7 +5,7 @@ def ghz_to_mhz(frequency):
   return frequency * 1000
 
 def kg_to_lbs(weight):
-  return weight * 0.45
+  return round(weight / 0.45)
 
 def lbs_to_kg(weight):
-  return weight / 0.45
+  return (weight * 0.45)
