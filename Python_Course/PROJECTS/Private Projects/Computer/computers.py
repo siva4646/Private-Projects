58bbadcbebb069cb3bc8d73e9509a0a67f12b46b
Michele Pipicelli <PIPICM25@smc.sa.edu.au>
2021-04-01 14:21:09 +1030

    Update
    Commit working/beta version of file.
    
    Unoptimized

--- Python_Course/PROJECTS/Private Projects/Computer/computers.py
+++ Python_Course/PROJECTS/Private Projects/Computer/computers.py
@@ -1,8 +1,26 @@
-graphics = ['GeForce', 'Nvidia']
+graphics = {
+    "geforce": "GeForce",
+    "nvidia": "Nvidia"
+}
 
+gpu = str(input())
 weight = float(input())
 model = input()
 cpufreq = int(input())
 
-# model = model.split(' ')
-# print(model)
+
+gpu.lower()
+if gpu in graphics:
+    gpu = graphics.get(gpu)
+    output = f'{cpufreq}MHz {gpu} {weight}kg {model}'
+    cpu = f'CPU Speed: {str(cpufreq)}MHz'
+    model = f'Model: {model}'
+    weight = f'Weight: {weight}kg'
+    gpu = f'GPU Type: {gpu}'
+    print(cpu)
+    print(weight)
+    print(gpu)
+    print(model)
+    print(output)
+else:
+  print('Please input either GeForce or Nvidia.')
