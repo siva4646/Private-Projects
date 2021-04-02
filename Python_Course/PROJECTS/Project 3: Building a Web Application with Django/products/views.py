4988aed62b0b568c6b9afb688c26b51d19dba432
Michele Pipicelli <michelepipicellii@gmail.com>
2021-04-02 18:41:29 +1030

    urls.py

---
+++ Python_Course/PROJECTS/Project 3: Building a Web Application with Django/products/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# /products call index() function
+# URL (Uniform Resource Locator) (Address)
+def index(request):
+    return HttpResponse("Hello, World!")
