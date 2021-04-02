b756de780be5e72dc7ac663de5c88ed5dda5bb05
Michele Pipicelli <michelepipicellii@gmail.com>
2021-04-02 21:15:22 +1030

    Major Files

--- Python_Course/PROJECTS/Project 3: Building a Web Application with Django/products/models.py
+++ Python_Course/PROJECTS/Project 3: Building a Web Application with Django/products/models.py
@@ -8,3 +8,9 @@ class Product(models.Model): # Inherit Model class
     image_url = models.CharField(max_length=2083)
 
 
+class Offer(models.Model):
+    code = models.CharField(max_length=10)
+    description = models.CharField(max_length=255)
+    discount = models.FloatField(max_length=5)
+
+
--- Python_Course/PROJECTS/Project 3: Building a Web Application with Django/products/views.py
+++ Python_Course/PROJECTS/Project 3: Building a Web Application with Django/products/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # /products call index() function
 # URL (Uniform Resource Locator) (Address)
 def index(request):
-    return HttpResponse("Hello, World!")
+    # Product.objects.save() Save/Update existing product
+    products = Product.objects.all()
+    return render(  request
+                  , 'index.html'
+                  , {'products': products})
 
 
 def new(request):
