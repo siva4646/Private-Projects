da6fb45cf2968ae06ae1ef38a8d223a216cccc83
Michele Pipicelli <michelepipicellii@gmail.com>
2021-04-08 18:14:18 +0930

    edureka

---
+++ eduraka/PROJECTS/index.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+p = Path()
+
+print('''
+Welcome to the index of the Python Projects Folder.
+Would you like to view the contents of the folder? (y or n)
+''')
+choice = input("").lower()
+if choice == 'y':
+    for item in p.glob("*"):
+        print(item)
+else:
+    print(f'{choice} entered... exiting...')
